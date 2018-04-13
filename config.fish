ccbdb67fd4302dccd1e5983e338d4a4044569f8e
Simon Korn <simon.korn@textalk.se>
2018-04-13 08:24:53 +0200

    default stuff for fzf

--- config.fish
+++ config.fish
@@ -2,3 +2,8 @@ set -x EDITOR nvim
 set -x VISUAL nvim
 set -x PATH $PATH /snap/bin
 set -x TERMINAL alacritty
+
+set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+set -x FZF_CTRL_T_COMMAND '$FZF_DEFAULT_COMMAND'
+set -x FZF_DEFAULT_OPTS "--color bg+:'#161c26'"
+
