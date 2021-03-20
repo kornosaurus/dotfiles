687e47bd4e98cba36346e5163de00d6c3d1c1292
kornosaurus <mail@simonkorn.se>
2021-03-20 17:43:34 +0100

    Neovim: Moved options to lua

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,6 +1,8 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap=true, silent=true }
 
+vim.g.mapleader = ' '
+
 -- LSP
 map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
 map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
@@ -8,8 +10,8 @@ map('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
 map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
 map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 map('i', '<C-h>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-map('n', '<space>dq', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-map('n', '<space>dd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-map('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-map('v', '<space>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
+map('n', '<leader>dq', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+map('n', '<leader>dd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+map('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+map('v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
--- nvim/lua/opts.lua
+++ nvim/lua/opts.lua
@@ -1,3 +1,6 @@
+local indent = 4
+local blend = 10
+
 -- global options
 vim.o.syntax = "on"
 vim.o.background = "dark"
@@ -6,6 +9,41 @@ vim.cmd [[
     silent! colorscheme nightfly
 ]]
 
+vim.o.grepprg = "rg --vimgrep"
+vim.o.grepformat = "%f:%l:%c:%m"
+vim.o.completeopt = "menuone,noinsert,noselect"
+vim.o.foldlevelstart = 99
+vim.o.updatetime = 100
+vim.o.scrolloff = 7
+vim.o.pumheight = 20
+vim.o.pumblend = blend
+vim.o.hidden = true
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.incsearch = true
+vim.o.lazyredraw = true
+vim.o.undodir = "~/.cache/undodir"
+vim.o.inccommand = "nosplit" -- Live substitute
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.showmode = false
+vim.o.listchars = "tab:  ,trail:·"
+vim.o.guicursor = ""
+
 -- window-local options
 vim.wo.number = true
 vim.wo.relativenumber = true
+vim.wo.wrap = false
+vim.wo.signcolumn = "yes"
+vim.wo.winblend = blend
+vim.wo.list = true
+
+-- buffer-local options
+vim.bo.spelllang = "en,sv"
+vim.bo.expandtab = true
+vim.bo.swapfile = false
+vim.bo.undofile = true
+vim.bo.shiftwidth = indent
+vim.bo.tabstop = indent
+vim.bo.smartindent = true
