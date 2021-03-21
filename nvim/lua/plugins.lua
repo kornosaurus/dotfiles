d3f26b251d0b8fd912a0daca879dec2c16c49620
kornosaurus <mail@simonkorn.se>
2021-03-21 21:28:47 +0100

    Neovim: self manage packer.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,32 +1,35 @@
+vim.cmd [[packadd packer.nvim]]
+
 require('packer').startup(function(use)
-  use {
-      'nvim-telescope/telescope.nvim',
-      requires = {'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim'}
-  }
+    use {'wbthomason/packer.nvim', opt = true}
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim'}
+    }
 
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-compe'
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'sbdchd/neoformat'
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-compe'
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'sbdchd/neoformat'
 
-  use 'tpope/vim-fugitive'
-  use 'airblade/vim-gitgutter'
+    use 'tpope/vim-fugitive'
+    use 'airblade/vim-gitgutter'
 
-  use 'tpope/vim-surround'
-  use 'rstacruz/vim-closer'
-  use 'SirVer/ultisnips'
-  use 'mattn/emmet-vim'
-  use 'terrortylor/nvim-comment'
+    use 'tpope/vim-surround'
+    use 'rstacruz/vim-closer'
+    use 'SirVer/ultisnips'
+    use 'mattn/emmet-vim'
+    use 'terrortylor/nvim-comment'
 
-  use 'vimwiki/vimwiki'
-  use 'junegunn/goyo.vim'
+    use 'vimwiki/vimwiki'
+    use 'junegunn/goyo.vim'
 
-  use 'skywind3000/asyncrun.vim'
-  use 'voldikss/vim-floaterm'
+    use 'skywind3000/asyncrun.vim'
+    use 'voldikss/vim-floaterm'
 
-  use 'ayu-theme/ayu-vim'
-  use 'pineapplegiant/spaceduck'
-  use 'bluz71/vim-nightfly-guicolors'
+    use 'ayu-theme/ayu-vim'
+    use 'pineapplegiant/spaceduck'
+    use 'bluz71/vim-nightfly-guicolors'
 end)
 
 -- Plugin Setup
@@ -42,16 +45,16 @@ require('nvim-treesitter.configs').setup {
 }
 
 require'compe'.setup {
-	enabled = true;
-	autocomplete = false;
+    enabled = true;
+    autocomplete = false;
 
-	source = {
-		path = true;
-		buffer = true;
-		calc = true;
-		nvim_lsp = true;
-		nvim_lua = true;
-	};
+    source = {
+        path = true;
+        buffer = true;
+        calc = true;
+        nvim_lsp = true;
+        nvim_lua = true;
+    };
 }
 
 -- Plugin Options
