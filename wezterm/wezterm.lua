04b54af7dd5b6241ac202ac6f4c37c326e61d629
kornosaurus <mail@simonkorn.se>
2024-01-11 15:43:26 +0100

    Wezterm colors

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,18 +1,18 @@
 local wezterm = require('wezterm')
--- local colors = require('lua/rose-pine-moon').colors()
--- local window_frame = require('lua/rose-pine-moon').window_frame()
+local colors = require('lua/rose-pine').colors()
+local window_frame = require('lua/rose-pine').window_frame()
 local act = wezterm.action;
 
 return {
-    force_reverse_video_cursor = false,
-    color_scheme = 'carbonfox',
-    -- colors = colors,
-    -- window_frame = window_frame,
+    -- force_reverse_video_cursor = false,
+    -- color_scheme = 'Kanagawa (Gogh)',
+    colors = colors,
+    window_frame = window_frame,
     font = wezterm.font('Monolisa'),
     harfbuzz_features = { 'zero', 'ss01' },
     freetype_load_flags = 'NO_HINTING',
     font_size = 14,
-    line_height = 1.3,
+    line_height = 1.2,
     initial_cols = 120,
     initial_rows = 32,
 
@@ -47,10 +47,10 @@ return {
         -- Splits
         { key = 's', mods = 'ALT|CTRL',  action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
         { key = 'v', mods = 'ALT|CTRL',  action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-        -- { key = 'j', mods = 'ALT',       action = act.EmitEvent('ActivatePaneDirection-down') },
-        -- { key = 'h', mods = 'ALT',       action = act.EmitEvent('ActivatePaneDirection-left') },
-        -- { key = 'k', mods = 'ALT',       action = act.EmitEvent('ActivatePaneDirection-up') },
-        -- { key = 'l', mods = 'ALT',       action = act.EmitEvent('ActivatePaneDirection-right') },
+        { key = 'j', mods = 'ALT|CTRL',  action = act.ActivatePaneDirection('Down') },
+        { key = 'h', mods = 'ALT|CTRL',  action = act.ActivatePaneDirection('Left') },
+        { key = 'k', mods = 'ALT|CTRL',  action = act.ActivatePaneDirection('Up') },
+        { key = 'l', mods = 'ALT|CTRL',  action = act.ActivatePaneDirection('Right') },
         { key = 'q', mods = 'SHIFT|ALT', action = act.CloseCurrentPane({ confirm = true }) },
 
         -- Panes
